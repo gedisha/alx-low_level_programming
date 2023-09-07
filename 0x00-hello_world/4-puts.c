7c236843888b41bb3fc4776ab24b863fd65b26ca
gedisha <gedishakatola@gmail.com>
2023-09-07 07:03:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+ * main - 4-puts.c
+ * *description - Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
+
