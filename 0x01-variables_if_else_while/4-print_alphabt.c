daeb4d9e9001b073893af70d38653e389b3b4faa
gedisha <gedishakatola@gmail.com>
2023-09-10 09:02:24 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,20 +2,22 @@
 
 
 /**
- *  main - program that prints the alphabet in lowercase and upper case
+ *  main - prints the alphabet in lower and upper case
  *
- *  Return: Always 0.
+ *  Return: Always 0
  */
 
 int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	putchar(letter);
+	for
+		(letter = 'a'; letter <= 'z'; letter++)
+			putchar(letter);
 
-	for (letter = 'A'; letter <= 'Z'; letter++)
-	putchar(letter);
+	for
+		(letter = 'A'; letter <= 'Z'; letter++)
+			putchar(letter);
 	putchar('\n');
 	return (0);
 }
